36263bdc529123a62b1a1e3396f6244e65d5cacf
denooy <89561213+denooy@users.noreply.github.com>
2022-10-02 20:18:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,80 @@ function toggle(source) {
     }
   }
 
+  function toggle(source) {
+    checkboxes = document.getElementsByName('ups');
+    for(var i=0, n=checkboxes.length;i<n;i++) {
+      checkboxes[i].checked = source.checked;
+    }
+  }
+
+  function uncheckUps(obj) {
+  	if (obj.checked == false) {
+  		document.getElementById("upsacct").checked = false;
+  	}
+  }
+
+  function toggle_visibility(dropship) {
+  	var e = document.getElementById("dropship");
+  	if (e.style.display == 'block')
+  		e.style.display = 'none';
+  	else
+  		e.style.display = 'block';
+  }
+
+  function uncheckScale(obj) {
+  	if (obj.checked == false) {
+  		document.getElementById("scale").checked = false;
+  	}
+  }
+
+  function uncheckCarriers(obj) {
+  	if (obj.checked == false) {
+  		document.getElementById("carrierlist").checked = false;
+  	}
+  }
+
+  $(document).ready(function() {    
+    $("#canadaship").click(function(){
+      $("#canada").slideToggle("fast");
+  });
+  $("#internationalship").click(function(){
+      $("#international").slideToggle("fast");
+  });
+      $("#carrierlist").click(function(){
+      $("#approved").slideToggle("fast");
+  });
+  $("#ltl").click(function(){
+    $("#approved").hide('fast');
+  });
+    $("#flatbed").click(function(){
+    $("#flatbedlist").slideToggle("fast");
+  });
+    $("#noCheck").click(function(){
+    $("#ifYes").hide('fast');
+  });
+    $("#yesCheck").click(function(){
+    $("#ifYes").show('fast');
+  });
+    $("#stock-no").click(function(){
+    $("#stock-amount").hide('fast');
+  });
+    $("#stock-yes").click(function(){
+    $("#stock-amount").show('fast');
+  });
+  $("#stock-no1").click(function(){
+    $("#free-amount").hide('fast');
+  });
+    $("#stock-yes1").click(function(){
+    $("#free-amount").show('fast');
+  });
+    $("#ltl").click(function(){
+    $("#arrangefreight").slideToggle("fast");
+  });
+    $("#dropship-no-amount").click(function(){
+    $("#dropship-amount").hide('fast');
+  });
+    $("#dropship-yes-amount").click(function(){
+    $("#dropship-amount").show('fast');
+  });
+});
