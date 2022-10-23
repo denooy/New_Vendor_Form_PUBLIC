dc6ca15f07fae76d50401a8d870322f447446801
denooy <89561213+denooy@users.noreply.github.com>
2022-10-23 14:54:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -122,6 +122,54 @@ function toggle(source) {
 });
 
 
+// BEGIN PHONE # INPUT FORMATTING
+
+const isNumericInput = (event) => {
+	const key = event.keyCode;
+	return ((key >= 48 && key <= 57) || 
+		(key >= 96 && key <= 105) 
+	);
+};
+
+const isModifierKey = (event) => {
+	const key = event.keyCode;
+	return (event.shiftKey === true || key === 35 || key === 36) || 
+		(key === 8 || key === 9 || key === 13 || key === 46) || 
+		(key > 36 && key < 41) ||
+		(
+		
+			(event.ctrlKey === true || event.metaKey === true) &&
+			(key === 65 || key === 67 || key === 86 || key === 88 || key === 90)
+		)
+};
+
+const enforceFormat = (event) => {
+
+	if(!isNumericInput(event) && !isModifierKey(event)){
+		event.preventDefault();
+	}
+};
+
+const formatToPhone = (event) => {
+	if(isModifierKey(event)) {return;}
+
+	const target = event.target;
+	const input = event.target.value.replace(/\D/g,'').substring(0,10);
+	const zip = input.substring(0,3);
+	const middle = input.substring(3,6);
+	const last = input.substring(6,10);
+
+	if(input.length > 6){target.value = `(${zip}) ${middle}-${last}`;}
+	else if(input.length > 3){target.value = `(${zip}) ${middle}`;}
+	else if(input.length > 0){target.value = `(${zip}`;}
+};
+
+const inputElement = document.getElementById('phonenumber');
+inputElement.addEventListener('keydown',enforceFormat);
+inputElement.addEventListener('keyup',formatToPhone);
+
+// END PHONE # INPUT FORMATTING
+
 // BEGIN CURRENCY INPUT FORMATTING
 
 $("input[data-type='currency']").on({
@@ -135,76 +183,53 @@ $("input[data-type='currency']").on({
 
 
 function formatNumber(n) {
-// format number 1000000 to 1,234,567
 return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
-
 function formatCurrency(input, blur) {
-// appends $ to value, validates decimal side
-// and puts cursor back in right position.
-
-// get input value
 var input_val = input.val();
 
-// don't validate empty input
 if (input_val === "") { return; }
 
-// original length
 var original_len = input_val.length;
 
-// initial caret position 
 var caret_pos = input.prop("selectionStart");
   
-// check for decimal
 if (input_val.indexOf(".") >= 0) {
 
-  // get position of first decimal
-  // this prevents multiple decimals from
-  // being entered
   var decimal_pos = input_val.indexOf(".");
 
-  // split number by decimal point
   var left_side = input_val.substring(0, decimal_pos);
   var right_side = input_val.substring(decimal_pos);
 
-  // add commas to left side of number
   left_side = formatNumber(left_side);
 
-  // validate right side
   right_side = formatNumber(right_side);
   
-  // On blur make sure 2 numbers after decimal
   if (blur === "blur") {
     right_side += "00";
   }
   
-  // Limit decimal to only 2 digits
   right_side = right_side.substring(0, 2);
 
-  // join number by .
   input_val = "$" + left_side + "." + right_side;
 
 } else {
-  // no decimal entered
-  // add commas to number
-  // remove all non-digits
+
   input_val = formatNumber(input_val);
   input_val = "$" + input_val;
-  
-  // final formatting
+
   if (blur === "blur") {
     input_val += ".00";
   }
 }
 
-// send updated string to input
 input.val(input_val);
 
-// put caret back in the right position
 var updated_len = input_val.length;
 caret_pos = updated_len - original_len + caret_pos;
 input[0].setSelectionRange(caret_pos, caret_pos);
 }
 
+// END CURRENCY INPUT FORMATTING
 
